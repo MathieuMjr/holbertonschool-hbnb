197ecd68844429f81e445b8f9969f07eb0d69384
Mathieu <mathieu.mjr@gmail.com>
2025-10-15 14:27:26 +0200

    added few comments and documentation

--- part2/hbnb/app/persistence/repository.py
+++ part2/hbnb/app/persistence/repository.py
@@ -29,25 +29,52 @@ class Repository(ABC):
 
 class InMemoryRepository(Repository):
     def __init__(self):
+        """
+        This function save an object into a dict
+        """
         self._storage = {}
 
     def add(self, obj):
+        """
+        This function retrieve an object
+        by its id
+        """
         self._storage[obj.id] = obj
 
     def get(self, obj_id):
+        """
+        This function retrieve an object
+        by its id
+        """
         return self._storage.get(obj_id)
 
     def get_all(self):
+        """
+        This function retrieve all object
+        of a kind that are know
+        """
         return list(self._storage.values())
 
     def update(self, obj_id, data):
+        """
+        This function udpate an object
+        if its id exist
+        """
         obj = self.get(obj_id)
         if obj:
-            obj.update(data)
+            obj.update(data)  # use update method from base class in models
 
     def delete(self, obj_id):
+        """
+        This method delete an object
+        """
         if obj_id in self._storage:
             del self._storage[obj_id]
 
     def get_by_attribute(self, attr_name, attr_value):
-        return next((obj for obj in self._storage.values() if getattr(obj, attr_name) == attr_value), None)
+        """
+        This function retrieve an object
+        through an attribute
+        """
+        return next(
+            (obj for obj in self._storage.values() if getattr(obj, attr_name) == attr_value), None)

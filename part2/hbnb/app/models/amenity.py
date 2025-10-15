bc122a2877dfec235797a870fe8f14d1ac992275
Mathieu <mathieu.mjr@gmail.com>
2025-10-15 17:26:02 +0200

    added a to dict method

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -5,3 +5,9 @@ class Amenity(BaseModel):
     def __init__(self, name):
         super().__init__()
         self.name = name
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }

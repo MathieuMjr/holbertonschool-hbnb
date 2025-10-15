336a624a8f77fac5ef12fd5d5c9c8fc41485fa48
Mathieu <mathieu.mjr@gmail.com>
2025-10-15 14:28:04 +0200

    created a to_dict method so API can return a specific way to display user

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -8,4 +8,17 @@ class User(BaseModel):
         self.first_name = first_name
         self.email = email
         self.is_admin = is_admin
-        self.places = []  # List to store related places²
+        self.places = []  # List to store related places
+
+    def to_dict(self):
+        """
+        This function allow to display some specific attribute of user.
+        Usefull to avoid to return passwords, for example (will be implemented
+        later)"""
+        return {
+            "id": self.id,
+            "last_name": self.last_name,
+            "first_name": self.first_name,
+            "email": self.email,
+            "is_admin": self.is_admin
+            }

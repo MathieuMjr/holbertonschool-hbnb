741242fe67332dda847a50a809e1d21785768474
Mathieu <mathieu.mjr@gmail.com>
2025-10-17 14:57:13 +0200

    work in progress turned into comments so there's no problem

--- part2/hbnb/tests/test_place.py
+++ part2/hbnb/tests/test_place.py
@@ -1,6 +1,8 @@
 from app.models.place import Place
 from app.models.user import User
 from app.models.review import Review
+import unittest
+from app import create_app
 
 
 def test_place_creation():
@@ -12,11 +14,12 @@ def test_place_creation():
         price=100,
         latitude=37.7749,
         longitude=-122.4194,
-        owner=owner,
+        owner_id=owner,
     )
 
     # Adding a review
-    review = Review(comment="Great stay!", rating=5, place=place, owner=owner)
+    review = Review(
+        comment="Great stay!", rating=5, place_id=place, user_id=owner)
     place.add_review(review)
 
     assert place.title == "Cozy Apartment"
@@ -27,3 +30,27 @@ def test_place_creation():
 
 
 test_place_creation()
+
+# --- UNITTEST - API
+
+# class TestPlaceEndpoints(unittest.TestCase):
+
+    # def setUp(self):
+    #     self.app = create_app()
+    #     self.client = self.app.test_client()
+
+    # def test_create_place(self):
+    #     response = self.client.post('/api/v1/users/', json={
+    #         "first_name": "Jane",
+    #         "last_name": "Doe",
+    #         "email": "jane.doe@example.com"
+    #     })
+    #     self.assertEqual(response.status_code, 201)
+
+    # def test_create_user_invalid_data(self):
+    #     response = self.client.post('/api/v1/users/', json={
+    #         "first_name": "",
+    #         "last_name": "",
+    #         "email": "invalid-email"
+    #     })
+    #     self.assertEqual(response.status_code, 400)

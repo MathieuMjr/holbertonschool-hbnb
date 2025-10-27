09cffc256f88b79a4d09f1f86219504c32251aa6
Mathieu <mathieu.mjr@gmail.com>
2025-10-27 15:42:25 +0100

    juste few comments so it helps me order my knowledges

--- part2/hbnb/config.py
+++ part2/hbnb/config.py
@@ -1,13 +1,16 @@
 import os
 
+
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
     DEBUG = False
 
+
+# hérite de config et surchage debut:
 class DevelopmentConfig(Config):
     DEBUG = True
 
-config = {
-    'development': DevelopmentConfig,
-    'default': DevelopmentConfig
-}
+
+# dictionnaire ou les clé sont associées à un type de config
+# dans run.py, on choisira le type de config souhaité
+config = {"development": DevelopmentConfig, "default": DevelopmentConfig}

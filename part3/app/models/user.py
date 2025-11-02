954b4392eae9728f1532524998796c418aeedf83
Mathieu <mathieu.mjr@gmail.com>
2025-11-02 17:26:32 +0100

    user.py is now written with flask sqlalchemy

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -1,17 +1,15 @@
 from .base import BaseModel
-from app.extensions import bcrypt
+from app.extensions import bcrypt, db
 
 
 class User(BaseModel):
-    def __init__(self, last_name, first_name, email, password, is_admin=False):
-        super().__init__()
-        self.last_name = last_name
-        self.first_name = first_name
-        self.email = email
-        self.is_admin = is_admin
-        self.password = password
-        self.places = []  # List to store related places
-        self.reviews = []
+    __tablename__ = 'users'
+
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(128), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
 
     def add_review(self, review):
         """Add a review to the place."""

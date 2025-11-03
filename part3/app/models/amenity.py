a5388620693327285c337402553eca4e07c7ef83
Mathieu <mathieu.mjr@gmail.com>
2025-11-03 15:40:04 +0100

    relationship

--- part3/app/models/amenity.py
+++ part3/app/models/amenity.py
@@ -1,11 +1,13 @@
 from .base import BaseModel
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from .place import place_amenity
 
 
 class Amenity(BaseModel):
     __tablename__ = 'amenities'
 
     name: Mapped[str] = mapped_column(nullable=False, unique=True)
+    places = relationship("Place", secondary=place_amenity, back_populates='amenities')
 
     def to_dict(self):
         return {

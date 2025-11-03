b87b1317322030b334d9eb4cd917d66072aa665b
Mathieu <mathieu.mjr@gmail.com>
2025-11-03 17:54:37 +0100

    written columns with another syntax (trying to fix creation test) + some coding style

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -1,16 +1,23 @@
 from .base import BaseModel
-from app.extensions import bcrypt, db
-from sqlalchemy.orm import relationship
+from app.extensions import bcrypt
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Boolean, String
 
 
 class User(BaseModel):
     __tablename__ = 'users'
 
-    first_name = db.Column(db.String(50), nullable=False)
-    last_name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(120), nullable=False, unique=True)
-    password = db.Column(db.String(128), nullable=False)
-    is_admin = db.Column(db.Boolean, default=False)
+    first_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120),
+        nullable=False,
+        unique=True)
+    password: Mapped[str] = mapped_column(String(128), nullable=False)
+    is_admin: Mapped[bool] = mapped_column(
+        Boolean,
+        default=False,
+        nullable=False)
     places = relationship('Place', back_populates="owner", lazy=True)
     reviews = relationship('Review', back_populates="user", lazy=True)
 
